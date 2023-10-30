5630226864d84f4dd2c70cda7987ecdca97d46ed
Bruno R. Pianca <brunorpdev@gmail.com>
2023-10-30 03:10:49 -0300

    feat: Pequenas melhorias

--- askapp/views.py
+++ askapp/views.py
@@ -7,14 +7,22 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'askapp/index.html')
+    contagem_perguntas = Question.objects.count()
+    return render(request, 'askapp/index.html', {'contagem_perguntas': contagem_perguntas})
 
 
 def pergunta(request):
     if request.method == 'POST':
         pergunta = request.POST.get('pergunta', '')
-        resposta = random.choice(Answer.objects.all())
-        Question.objects.create(text=pergunta, answer=resposta)
-        return render(request, 'askapp/pergunta.html', {'resposta': resposta})
+        if not pergunta:
+            return HttpResponse('Pergunta não informada', status=400)
+        pergunta_existe = Question.objects.filter(text=pergunta).exists()
+        if pergunta_existe:
+            resposta = "Já respondi essa pergunta!"
+        else:
+            resposta = random.choice(Answer.objects.all())
+            Question.objects.create(text=pergunta, answer=resposta)
+        return render(request, 'askapp/pergunta.html', {'resposta': resposta,
+                                                        'contagem_perguntas': Question.objects.count()})
     else:
         return HttpResponse('Método não permitido', status=405)
